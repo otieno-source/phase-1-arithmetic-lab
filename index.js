aa43de2306fa911e4fbfcbaa858f50bad0174162
collins ouma otieno <otiencollins0@gmail.com>
2024-06-28 16:36:20 +0300

    phase_1 arthmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = (num1*num2)
+console.log(multiply)
+
+
+let x = (0)
+let random = (x+1)
+console.log(random)
+
+let num3 = 12
+let num4 = 3
+let mod = (num3/num4)
+console.log(mod)
+
+let y = 19
+let max = (y+1)
+console.log(max)
